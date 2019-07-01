e08ae570cf989a814ae07d9adbb0bf35081514c5
akmphyu <akmphyu@gmail.com>
2019-07-01 12:04:24 +0630

    Initial Commit

---
+++ ArtableAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Aye Kaung Mya Phyu on 1/7/19.
+//  Copyright © 2019 Aye Kaung Mya Phyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
